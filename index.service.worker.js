dee79e6548f03c5cefed885347c645e495a71dd6
Recraz <95542186+Recraz@users.noreply.github.com>
2026-03-20 17:33:58 +0100

    10

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774023967|4585053132';
+const CACHE_VERSION = '1774024432|5049468261';
 /** @type {string} */
 const CACHE_PREFIX = 'Otherworld-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
